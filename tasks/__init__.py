51b37c403f74bb42a4a99df4fb81bb7c33eb3c53
okatsn <okatsn@gmail.com>
2023-03-03 14:05:50 +0800

    Disable not-in-use inv commands

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,6 +4,6 @@ from tasks import env, nb, style, test
 
 ns = Collection()
 ns.add_collection(env)
-ns.add_collection(nb)
+# ns.add_collection(nb)
 ns.add_collection(style)
-ns.add_collection(test)
+# ns.add_collection(test)
