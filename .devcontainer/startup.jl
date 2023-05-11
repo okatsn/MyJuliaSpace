e3ed3cae48f6cc50626a5d7a8a76c129d6fccbd1
okatsn <okatsn@gmail.com>
2023-05-11 11:33:13 +0800

    not using Revise by default

--- .devcontainer/startup.jl
+++ .devcontainer/startup.jl
@@ -1,6 +1,6 @@
 atreplinit() do repl # [Automatically start with Julia](https://kristofferc.github.io/OhMyREPL.jl/latest/installation/#Automatically-start-with-Julia.)
     try
-        @eval using OhMyREPL, Revise
+        @eval using OhMyREPL
     catch e
         @warn "error while importing OhMyREPL" e
     end
